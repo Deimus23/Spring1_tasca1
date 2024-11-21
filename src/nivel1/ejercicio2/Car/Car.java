3aec25d1b0f9711d65b6baaf5a6acb7927d34e8e
Antonio Carrasco <carrasco.antonio.233@gmail.com>
2024-11-21 17:26:46 +0100

    ejercicio2

--- src/nivel1/ejercicio2/Car/Car.java
+++ src/nivel1/ejercicio2/Car/Car.java
@@ -1,4 +1,18 @@
 package nivel1.ejercicio2.Car;
 
 public class Car {
+  private static final String brand="Toyota";
+  private  static String model;
+  private final int power;
+
+    public Car(String model,int power) {
+        this.model=model;
+        this.power = power;
+    }
+    public static void brake(){
+        System.out.println("The car are braking");
+    }
+    public void accelerate(){
+        System.out.println("The car are accelerating");
+    }
 }
--- src/nivel1/ejercicio2/main/main.java
+++ src/nivel1/ejercicio2/main/main.java
@@ -1,4 +1,14 @@
 package nivel1.ejercicio2.main;
 
+import nivel1.ejercicio2.Car.Car;
+
 public class main {
+
+    public static void main(String[]args) {
+        Car car = new Car("yaris", 125);
+        Car.brake();
+        car.accelerate();
+    }
 }
+
+
